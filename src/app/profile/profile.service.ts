5c871df070b0ca6246d98ce0208a200b3092fe8f
beingmartinbmc <beingmartinbmc@users.noreply.github.com>
2021-01-10 22:18:37 +0530

    Changed data and added projects

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class ProfileService {
-    baseUrl = environment.baseUrl;
+  providedIn: 'root'
+})
+export class ProfileService {
+  baseUrl = environment.baseUrl;
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+  }
 
   contactus(data: any): Observable<any> {
-    return this.http.post('http://localhost:4200/contact',data);
+    return this.http.post('http://localhost:4200/contact', data);
   }
-  }
+}
