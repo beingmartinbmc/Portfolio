ee3e605438a626c693d78ae3fcccafc991ce9561
beingbmc12 <beingbmc12@gmail.com>
2021-01-22 17:39:23 +0530

    Changed resume link

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,8 +10,8 @@ export class ProfileComponent implements OnInit {
 
   myStyle: object = {};
   myParams: object = {};
-  width: number = 100;
-  height: number = 100;
+  width = 100;
+  height = 100;
 
   constructor(
     private spinner: NgxSpinnerService
@@ -30,14 +30,14 @@ export class ProfileComponent implements OnInit {
 
   particles2() {
     this.myStyle = {
-      'position': 'fixed',
-      'width': '100%',
-      'height': '100%',
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
       'z-index': -1,
-      'top': 0,
-      'left': 0,
-      'right': 0,
-      'bottom': 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
     };
 
     this.myParams = {
@@ -57,14 +57,14 @@ export class ProfileComponent implements OnInit {
 
   particles() {
     this.myStyle = {
-      'position': 'fixed',
-      'width': '100%',
-      'height': '100%',
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
       'z-index': -1,
-      'top': 0,
-      'left': 0,
-      'right': 0,
-      'bottom': 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
     };
 
     this.myParams = {
