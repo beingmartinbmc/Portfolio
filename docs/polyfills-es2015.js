46501f5802644236609161d14674c4e7db709608
Ankit Sharma <beingbmc12@gmail.com>
2021-06-11 16:29:01 +0530

    Added timeline for experience

--- docs/polyfills-es2015.js
+++ docs/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\marti\WebstormProjects\Portfolio\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! E:\Frontend\Portfolio\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
