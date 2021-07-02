ef93965bce89927928f795e8546c36fb9c79de33
beingmartinbmc <beingmartinbmc@users.noreply.github.com>
2021-07-02 10:13:21 +0530

    Added dynamic DOJ

--- src/app/profile/experience/experience.component.ts
+++ src/app/profile/experience/experience.component.ts
@@ -1,16 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TimeService} from '../../time.service';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
-export class ExperienceComponent implements OnInit {
+export class ExperienceComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  constructor(private timeService: TimeService) {
   }
 
+  numberOfMonths: number;
+  interval: any;
+
+  private refreshData() {
+    this.timeService.getNumberOfMonths()
+      .subscribe(data => {
+        this.numberOfMonths = data;
+      });
+  }
+
+
   ngOnInit(): void {
+    this.refreshData();
+    this.interval = setInterval(() => {
+      this.refreshData();
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 
 }
---
+++ src/app/time.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimeService {
+
+  constructor() {
+  }
+
+  public getNumberOfMonths(): Observable<number> {
+    const joinedDate = new Date('05/26/2021');
+    const currentDate = new Date();
+    const difference = currentDate.getTime() - joinedDate.getTime();
+    const days = difference / (1000 * 3600 * 24);
+    return of(Math.floor(days / 30));
+  }
+}
